b186fd0d34dc21ecf99e8a6334494164f284a87c
marius david <mariusdavid@laposte.net>
2020-07-06 19:37:05 +0200

    use path in batch.py

--- asmpatch/batch.py
+++ asmpatch/batch.py
@@ -12,37 +12,36 @@ def batchpatch(
     gld_path,
     gpp_path,
     end_offset,
-    input_name,
+    input_output_files,
     linker_files = [],
-    out_prefix = "",
-    out_suffix = "_diff.txt",
-    input_prefix = "",
-    input_suffix = ".asm"
 ):
+
+    input_files = []
+    output_files = []
+
+    for (input, output) in input_output_files:
+        input_files.append(input)
+        output_files.append(output)
 
     globals = read_linker_files(linker_files)
 
-    input_path = []
-    for name in input_name:
-        input_path.append(input_prefix + name + input_suffix)
-
     splited = []
     print("reading/splitting the different function")
-    for file_path in input_path:
-        content = read_file(file_path)
-        dir_path = os.path.dirname(file_path)
+    for input_path in input_files:
+        content = read_file(input_path)
+        dir_path = os.path.dirname(input_path)
         parsed = parse_asm(content, dir_path, gpp_path)
         splited.append(split_part(parsed))
 
     # generate object file
     print("generating the object file")
     objects = []
-    for (patch, name) in zip(splited, input_name):
+    for (patch, input_path) in zip(splited, input_files):
         actual_object = []
         loop_counter = 0
         for section in patch:
             actual_object.append({
-                "object_bin": generate_object(section, gas_path, name, loop_counter),
+                "object_bin": generate_object(section, gas_path, input_path, loop_counter),
                 "data": section
             })
             start = actual_object[-1]["data"]["start"]
@@ -63,11 +62,11 @@ def batchpatch(
     # TODO: check and merge overwrite
     print("generating map file (metadata) (undefined reference can be ignored)")
     objects_pass_two = []
-    for (patch, name) in zip(objects, input_name):
+    for (patch, input_path) in zip(objects, input_files):
         objects_pass_two.append([])
         loop_nb = 0
         for section in patch:
-            meta = get_metadata_object(section["data"], section["object_bin"], name, loop_nb, gld_path, actual_end_offset)
+            meta = get_metadata_object(section["data"], section["object_bin"], input_path, loop_nb, gld_path, actual_end_offset)
             actual_section_data = {
                 "object_bin": section["object_bin"],
                 "start": meta["correct_start"],
@@ -80,14 +79,13 @@ def batchpatch(
 
     # generate patches files
     print("generating patches (undefined reference should *not* be ignored)")
-    for (file_name, datas) in zip(input_name, objects_pass_two):
-        print("  for {}".format(file_name))
+    for (input_path, output_path, datas) in zip(input_files, output_files, objects_pass_two):
+        print("  from {} to {}".format(input_path, output_path))
         diffs = {}
         loop_nb = 0
         for data in datas:
-            patch_bin = generate_binary_patch(data["object_bin"], globals, data["start"], file_name, loop_nb, gld_path)
+            patch_bin = generate_binary_patch(data["object_bin"], globals, data["start"], input_path, loop_nb, gld_path)
             diffs[data["start"]] = list(struct.unpack("B"*len(patch_bin), patch_bin))
             loop_nb += 1
-        out_file_path = out_prefix + file_name + out_suffix
-        with open(out_file_path, "w") as f:
+        with open(output_path, "w") as f:
             f.write(yaml.dump({"sys/main.dol": diffs}, Dumper=yaml.CDumper))
--- main.py
+++ main.py
@@ -6,14 +6,18 @@ gas = "/nix/store/wqdf40jb3787wvi004rm1k6m2i1fsgyv-powerpc-none-eabi-stage-final
 gld = "/nix/store/wqdf40jb3787wvi004rm1k6m2i1fsgyv-powerpc-none-eabi-stage-final-gcc-debug-wrapper-9.3.0/bin/powerpc-none-eabi-ld"
 gpp = "/nix/store/wqdf40jb3787wvi004rm1k6m2i1fsgyv-powerpc-none-eabi-stage-final-gcc-debug-wrapper-9.3.0/bin/powerpc-none-eabi-g++"
 
+def transform_to_entry(name):
+    return ("./patches/"+name+".asm", "./build/"+name+"_diff.txt")
+
 print("generating ...")
 batchpatch(
     gas,
     gld,
     gpp,
-    "0x805954bc", #offset of the end of the file
-    [ "custom_funcs", "remove_optimization_for_freecam_ntsc", "apply_changes" ],
-    input_prefix = "./patches/",
-    out_prefix = "./build/"
+    int("805954bc", 16), #offset of the end of the file
+    #[ "custom_funcs", "remove_optimization_for_freecam_ntsc", "apply_changes", "include_cpp" ],
+    #[ "include_cpp" ],
+    map(transform_to_entry, ["custom_funcs", "remove_optimization_for_freecam_ntsc", "apply_changes", "include_cpp"]),
+    linker_files = [ "patches/spyro06_ntsc.ld" ],
 )
 print("done !")
